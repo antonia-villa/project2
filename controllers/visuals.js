6592c00f687de4195251879b3bd8ea8ca2f4be10
ANTONIA-VILLA”git config --global user.email AV.FRACTALWORK@GMAIL.COMSgit config --global user.email AV.FRACTALWORK@GMAIL.COM <av.fractalwork@gmail.com>
2018-01-09 10:48:21 -0800

    updated file names

--- controllers/visuals.js
+++ controllers/visuals.js
@@ -3,11 +3,20 @@ var router = express.Router();
 var async = require('async');
 var db = require('../models');
 
+router.get('/new', function(req, res){
+	db.user.findAll({
+		include: [db.user]
+	});
+
+	res.render('visual/new');
+
+});
+
 
 //res.status(status).send(body)
 router.post('/', function(req, res){
 	console.log('body is', req.body);
-	res.redirect('/visual/new');
+	res.redirect('visual/visual');
 	// db.author.create(req.body).then(function(createdAuthor){
 	// 	res.redirect('/authors/'+ createdAuthor.id);
 	// }).catch(function(err){
