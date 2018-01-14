85b818e5e84fe99af35d8a8c187b1eb0dd5e7db4
ANTONIA-VILLA”git config --global user.email AV.FRACTALWORK@GMAIL.COMSgit config --global user.email AV.FRACTALWORK@GMAIL.COM <av.fractalwork@gmail.com>
2018-01-14 08:47:21 -0800

    commit

--- index.js
+++ index.js
@@ -47,6 +47,4 @@ app.get('/', function(req, res){
 	res.render('home');
 });
 
-app.listen(3000, function(){
-    console.log("info",'Server is running at port : ' + 3000);
-});
+app.listen(process.env.PORT || 3000);
